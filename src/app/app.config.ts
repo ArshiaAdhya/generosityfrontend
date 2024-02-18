15f5343051fb68798b666b14b211f1462358b77d
Om Swami <omswami2004@gmail.com>
2024-02-18 12:21:43 +0530

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"genbackend","appId":"1:111630538013:web:636c09da99b52d3c71bdf4","storageBucket":"genbackend.appspot.com","apiKey":"AIzaSyARBVjspDEzSxpJz7WaTU-KZHsD0RugPfE","authDomain":"genbackend.firebaseapp.com","messagingSenderId":"111630538013","measurementId":"G-72GW2PKEVX"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideStorage(() => getStorage()))]
 };

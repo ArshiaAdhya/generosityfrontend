c0b11db20951d3aa695bb20b0573590189e929b7
Om Swami <omswami2004@gmail.com>
2024-02-18 10:53:22 +0530

    added firebase basics

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+
+        apiKey: "AIzaSyARBVjspDEzSxpJz7WaTU-KZHsD0RugPfE",
+        authDomain: "genbackend.firebaseapp.com",
+        projectId: "genbackend",
+        storageBucket: "genbackend.appspot.com",
+        messagingSenderId: "111630538013",
+        appId: "1:111630538013:web:636c09da99b52d3c71bdf4",
+        measurementId: "G-72GW2PKEVX"
+    }
+};
